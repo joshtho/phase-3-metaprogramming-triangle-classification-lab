de93c2aac46d66ea83597ac399938f4bd0c82956
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-10-20 18:29:58 -0500

    commit

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c, :equilateral, :isosceles, :scalene
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind 
+    if a == b && a == c 
+      @equilateral = true 
+    elsif a == b || a == c || b == c 
+      @isosceles = true 
+      elsif a != b && a != c && b != c
+      @scalene = true
+      else 
+    end
+  end
+  
+  class TriangeError < StandardError
+    def message 
+      "Triangle Error, try again"
+    end
+  end
 end
